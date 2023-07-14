7bd2bb505b3ed5bb40e9003431ee633d9b917751
zacharymor <morzachary@gmail.com>
2023-07-14 17:18:26 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>Start building your awesome React application!</p>
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming your root component is named App
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
