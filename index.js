10b6301e912c1ada5fa31582816bc8267e82d975
zacharymor <morzachary@gmail.com>
2023-10-02 20:21:11 -0400

    Fixed Spotify integration

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: process.env.REDIRECT_URI || 'http://localhost:3000/callback'
+  redirectUri: process.env.REDIRECT_URI || 'http://listen2.app/callback'
 });
 
 // Express session setup
